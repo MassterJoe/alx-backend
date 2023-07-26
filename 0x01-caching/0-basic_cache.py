d2f2143485e7bcc02ec1c6bfe3133f5177628a5f
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-26 14:13:56 -0400

    great

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,12 +12,14 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        """ it puts values into the dictionary"""
         if key is None or item is None:
             return
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ It retrieves the values"""
         if key is not None:
             value = self.cache_data.get(key)
             return value
