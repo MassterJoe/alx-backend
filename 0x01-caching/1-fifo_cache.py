7f65f8075e91adc8b8570face360225b9eb44ac9
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-31 10:59:19 -0400

    great

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Create a class BasicCache that inherits
-from BaseCaching and is a caching system """
+"""
+Create a class FIFOCache
+"""
 
 
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ It inherits from BasicCaching """
+class FIFOCache(BaseCaching):
+    """ It inherits from the parent class BaseCaching  """
     def __init__(self):
         super().__init__()
 
@@ -15,11 +16,13 @@ class BasicCache(BaseCaching):
         """ it puts values into the dictionary"""
         if key is None or item is None:
             return
-        else:
-            self.cache_data[key] = item
+        self.cache_data[key] = item
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            key, value = self.cache_data.popitem()
+            print("DISCARD: {}".format(key))
 
     def get(self, key):
-        """ It retrieves the values"""
+        """ gets the value of the dict key"""
         if key is not None:
             value = self.cache_data.get(key)
             return value
