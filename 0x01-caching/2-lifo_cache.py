e08707eb3d7a242da71e122e644f11331982586e
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-31 12:49:49 -0400

    cool

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Create a class LIFOCache
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ It inherits from the parent class BaseCaching  """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ it puts values into the dictionary"""
+        if key is None or item is None:
+            return
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+            last_item = self.cache_data.popitem()[0]
+            print("DISCARD: {}".format(last_item))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ gets the value of the dict key"""
+        if key is not None:
+            value = self.cache_data.get(key)
+            return value
