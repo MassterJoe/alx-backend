b9ecc374ce5fbdd1a834bda60050f00de1ba7feb
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-04 09:31:34 -0400

    cool

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""  Create a single / route and an index.html template
+ that simply outputs “Welcome to Holberton” as page title (<title>)
+and “Hello world” as header (<h1>)."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
