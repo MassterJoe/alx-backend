5a99f464119309bf7ea6acbebe899048bdd5b0cc
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-04 08:49:28 -0400

    cool

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,12 +4,29 @@
 and “Hello world” as header (<h1>)."""
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
 
 
+class Config:
+    app.config['LANGUAGES'] = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
 @app.route('/')
-@app.route('/index')
-def index():
+def index1():
     """ it returns 0-index.html"""
     return render_template('0-index.html')
+
+
+@app.route('/index2')
+def index2():
+    return render_template('1-index.html')
