5595722dd030be24725c28dcb64a824b10c126ae
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-04 09:23:19 -0400

    cool1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-"""babel = Babel(app)"""
+babel = Babel(app)
 
-"""
+
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -12,25 +12,15 @@ class Config:
 
 
 app.config.from_object(Config)
-"""
 
 
 @app.route('/')
 def index1():
     """ Renders 0-index.html with custom data """
-    page_title = "Welcome to Holberton"
-    header = "Hello world"
-    return render_template('0-index.html',
-                           page_title=page_title, header=header)
+    return render_template('0-index.html')
 
 
-"""
 @app.route('/index2')
 def index2():
     """ Renders 1-index.html with custom data """
-    page_title = "This is the second page"
-    header = "This is the second page"
-    return render_template('1-index.html',
-                           page_title=page_title,
-                           header=header)
-"""
+    return render_template('1-index.html')
