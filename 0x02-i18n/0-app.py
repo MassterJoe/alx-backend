8e51a315fd04ad77d22005aa22345bb9918c5673
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-04 09:29:19 -0400

    cool

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,26 +1,13 @@
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
 
 
 @app.route('/')
-def index1():
+def get_index():
     """ Renders 0-index.html with custom data """
     return render_template('0-index.html')
 
 
-@app.route('/index2')
-def index2():
-    """ Renders 1-index.html with custom data """
-    return render_template('1-index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
