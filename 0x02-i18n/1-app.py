67f1cb1b2a6d45413f8225ca1838b45d8814cf3a
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-04 09:36:01 -0400

    coolest

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+""" configure the flask app and create a babe; object"""
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
+    """ config class"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -14,11 +18,10 @@ class Config:
 app.config.from_object(Config)
 
 
-@app.route('/index2')
+@app.route('/')
 def get_index():
     """ Renders 1-index.html with custom data """
     return render_template('1-index.html')
-
 
 
 if __name__ == '__main__':
