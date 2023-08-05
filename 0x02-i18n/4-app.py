a858f2e56b35b0410cfb6c8960c8f4c1cdc176b3
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-05 04:30:55 -0400

    cool

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,6 +21,10 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ get locale function"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
