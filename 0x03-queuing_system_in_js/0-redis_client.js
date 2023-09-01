e427d721b792c2366231eebfc97ee7115eea39eb
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-09-01 16:33:38 -0400

    cool

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+    host: 'localhost',
+    port: 6379,
+});
+
+client.on('connect', function (){
+    console.log('Redis client connected to the server');
+}).on('error', (err) => {
+    console.error(`Redis client not connected to the server: ${err}`);
+});
